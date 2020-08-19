5c5748f65195b0e7305f777d55fcfffd8ced2f91
Vincent Emonet <vincent.emonet@gmail.com>
2020-08-19 09:22:39 +0200

    remove useless blocks from run_function.py

--- tests/run_function.py
+++ tests/run_function.py
@@ -4,12 +4,8 @@ from openpredict.openpredict_api import get_predict
 ## Run it:
 # python3 tests/run_function.py
 
-## Get prediction for Drug
-
-entity = 'DB00394'
-
 # Call get predict from API for a DRUG
-prediction_result = get_predict('DB00394', 'drug', 'disease')
+prediction_result = get_predict('DRUGBANK:DB00394')
 
 print('Results:')
 print(prediction_result)
