181e8f522b3c634238a6d514afd2a68c4affedb4
Vincent Emonet <vincent.emonet@gmail.com>
2020-08-25 20:29:17 +0200

    update run_function

--- tests/run_function.py
+++ tests/run_function.py
@@ -3,16 +3,20 @@ from openpredict.openpredict_api import get_predict
 from openpredict.openpredict_omim_drugbank import get_drug_disease_classifier
 from datetime import datetime
 
+## Run it:
+# python3 tests/run_function.py
+
 time_start = datetime.now()
 clf, scores = get_drug_disease_classifier()
 
-## Run it:
-# python3 tests/run_function.py
+time_build = datetime.now()
 
 # Call get predict from API for a DRUG
 prediction_result = get_predict('DRUGBANK:DB00394')
 
-print('PredictRuntime: ' + str(datetime.now() - time_start))
+print('Build runtime: ' + str(time_build - time_start))
+print('Predict runtime: ' + str(datetime.now() - time_build))
+print('Total runtime: ' + str(datetime.now() - time_start))
 
-print('Results:')
-print(prediction_result)
+# print('Results:')
+# print(prediction_result)
