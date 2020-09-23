58265f934cf675c18714d7d46c9a5d5c524f3cb6
Vincent Emonet <vincent.emonet@gmail.com>
2020-09-23 19:42:39 +0200

    add reset_models.sh to reset models and metadata to the latest git commit

--- reset_models.sh
+++ reset_models.sh
@@ -1,3 +1,5 @@
+# Reset models, and metadata to the last git commit
+
 git checkout HEAD -- openpredict/data/openpredict-metadata.ttl
 git checkout HEAD -- openpredict/data/features/drug_disease_dataframes.joblib
 git checkout HEAD -- openpredict/data/models/drug_disease_model.joblib
