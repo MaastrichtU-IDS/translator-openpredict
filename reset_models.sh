a927fdc2eea37b3c7bc4366e02c7cf9e8d023535
Vincent Emonet <vincent.emonet@gmail.com>
2020-11-26 17:55:01 +0100

    update reset model

--- reset_models.sh
+++ reset_models.sh
@@ -1,5 +1,7 @@
 # Reset models, and metadata to the last git commit
 
-git checkout HEAD -- openpredict/data/openpredict-metadata.ttl
-git checkout HEAD -- openpredict/data/features/openpredict-baseline-omim-drugbank.joblib
-git checkout HEAD -- openpredict/data/models/openpredict-baseline-omim-drugbank.joblib
+find ./data ! -name 'initial-openpredict-metadata.ttl' -exec sudo rm -rf {} +
+
+# git checkout HEAD -- openpredict/data/openpredict-metadata.ttl
+# git checkout HEAD -- openpredict/data/features/openpredict-baseline-omim-drugbank.joblib
+# git checkout HEAD -- openpredict/data/models/openpredict-baseline-omim-drugbank.joblib
