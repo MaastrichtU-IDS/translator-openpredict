c75e7c939988b739db458518469fe5b27624b541
Vincent Emonet <vincent.emonet@gmail.com>
2021-01-11 13:09:42 +0100

    update Translator Reasoner API version from 0.9.1 to 1.0.0-beta

--- openpredict/reasonerapi_parser.py
+++ openpredict/reasonerapi_parser.py
@@ -8,43 +8,56 @@ def typed_results_to_reasonerapi(reasoner_query, model_id):
     :param: reasoner_query Query from Reasoner API
     :return: Results as ReasonerAPI object
     """
+    # Example TRAPI message: https://github.com/NCATSTranslator/ReasonerAPI/blob/master/examples/Message/simple.json
     query_graph = reasoner_query["message"]["query_graph"]
+
+    try:
+        n_results = reasoner_query["message"]["n_results"]
+    except:
+        print('n_results retrieve failed')
+        n_results = None
+
     try:
         min_score = float(reasoner_query["message"]["query_options"]["min_score"])
     except:
         print('min score retrieve failed')
         min_score=None
+        
     try:
         max_score = float(reasoner_query["message"]["query_options"]["max_score"])
     except: 
         print('max score retrieve failed')
         max_score=None
     query_plan = {}
+
     # Parse the query_graph to build the query plan
-    for qg_edge in query_graph["edges"]:
+    # TODO: iterate object instead of array
+    for edge_id, qg_edge in query_graph["edges"].items():
+    # for qg_edge in query_graph["edges"]:
         # Build dict with all infos of associations to predict 
-        query_plan[qg_edge['id']] = {
-            'association_type': qg_edge['type'],
-            'qg_source_id': qg_edge['source_id'],
-            'qg_target_id': qg_edge['target_id']
+        query_plan[edge_id] = {
+            'predicate': qg_edge['predicate'],
+            'qg_source_id': qg_edge['subject'],
+            'qg_target_id': qg_edge['object']
         }
 
         # Get the nodes infos in the query plan object
-        for node in query_graph['nodes']:
-            if node['id'] == qg_edge['source_id'] or node['id'] == qg_edge['target_id']:
+        for node_id, node in query_graph["nodes"].items():
+        # for node in query_graph['nodes']:
+            if node_id == qg_edge['subject'] or node_id == qg_edge['object']:
                 if 'curie' in node:
                     # The node with curie is the association's "from"
-                    query_plan[qg_edge['id']]['from_kg_id'] = node['curie']
-                    query_plan[qg_edge['id']]['from_qg_id'] = node['id']
-                    query_plan[qg_edge['id']]['from_type'] = node['type'].lower()
+                    query_plan[edge_id]['from_kg_id'] = node['curie']
+                    query_plan[edge_id]['from_qg_id'] = node_id
+                    query_plan[edge_id]['from_type'] = node['category'].lower()
                 else:
                     # The node without curie is the association's "to"
-                    query_plan[qg_edge['id']]['to_qg_id'] = node['id']
-                    query_plan[qg_edge['id']]['to_type'] = node['type'].lower()
+                    query_plan[edge_id]['to_qg_id'] = node_id
+                    query_plan[edge_id]['to_type'] = node['category'].lower()
 
         # TODO: edge type should be required
 
-    knowledge_graph = {'nodes': [], 'edges': []}
+    knowledge_graph = {'nodes': {}, 'edges': {}}
     node_dict = {}
     query_results = []
     kg_edge_count = 0
@@ -78,98 +91,61 @@ def typed_results_to_reasonerapi(reasoner_query, model_id):
             if 'label' in association['target'] and association['target']['label']:
                 node_dict[association['target']['id']]['label'] = association['target']['label']
 
+            # TODO: make it dynamic?
+            edge_association_type = 'biolink:ChemicalToDiseaseOrPhenotypicFeatureAssociation'
+            provided_by = 'Translator OpenPredict model ' + model_id
+
             edge_dict = {
-                'id': edge_kg_id,
-                'type': query_plan[edge_qg_id]['association_type'],
-                'score': association['score'] }
+                'association_type': edge_association_type,
+                'predicate': query_plan[edge_qg_id]['predicate'],
+                'has_confidence_level': association['score'],
+                "provided_by": provided_by,
+                # "relation": "OBOREL:0002606",
+            }
 
             # Map the source/target of query_graph to source/target of association
             if association['source']['type'] == query_plan[edge_qg_id]['from_type']:
-                edge_dict['source_id'] = association['source']['id']
-                edge_dict['target_id'] = association['target']['id']    
+                edge_dict['subject'] = association['source']['id']
+                edge_dict['object'] = association['target']['id']    
             else: 
-                edge_dict['source_id'] = association['target']['id']
-                edge_dict['target_id'] = association['source']['id']    
+                edge_dict['subject'] = association['target']['id']
+                edge_dict['object'] = association['source']['id']    
 
             # Add the association in the knowledge_graph as edge
             # Use the type as key in the result association dict (for IDs)
-            knowledge_graph['edges'].append(edge_dict)
+            knowledge_graph['edges'][edge_kg_id] = edge_dict
 
             # Add the bindings to the results object
-            result = {'edge_bindings': [], 'node_bindings': []}
-            result['edge_bindings'].append(
+            result = {'edge_bindings': {}, 'node_bindings': {}}
+            result['edge_bindings'][edge_qg_id] = [
                 {
-                    "kg_id": edge_kg_id,
-                    'qg_id': edge_qg_id
+                    "id": edge_kg_id
                 }
-            )
-            result['node_bindings'].append(
+            ]
+            result['node_bindings'][query_plan[edge_qg_id]['from_qg_id']] = [
                 {
-                    "kg_id": association['source']['id'],
-                    'qg_id': query_plan[edge_qg_id]['from_qg_id']
-                })
-            result['node_bindings'].append(
+                    "id": association['source']['id']
+                }
+            ]
+            result['node_bindings'][query_plan[edge_qg_id]['to_qg_id']] = [
                 {
-                    "kg_id": association['target']['id'],
-                    'qg_id': query_plan[edge_qg_id]['to_qg_id']
-                })
+                    "id": association['target']['id']
+                }
+            ]
             query_results.append(result)
+
             kg_edge_count += 1
+            if kg_edge_count == n_results:
+                break
+
 
     # Generate kg nodes from the dict of nodes + result from query to resolve labels
     for node_id, properties in node_dict.items():
         node_to_add = {
-            'id': node_id,
-            'type': properties['type'],
+            'category': properties['type'],
             }
         if 'label' in properties and properties['label']:
             node_to_add['name'] = properties['label']
 
-        knowledge_graph['nodes'].append(node_to_add)
+        knowledge_graph['nodes'][node_id] = node_to_add
     return {'knowledge_graph': knowledge_graph, 'query_graph': query_graph, 'results': query_results}
-
-## Sample Reasoner query
-# https://github.com/broadinstitute/molecular-data-provider/blob/master/reasonerAPI/python-flask-server/openapi_server/controllers/molepro.py#L30
-# https://smart-api.info/ui/912372f46127b79fb387cd2397203709#/0.9.2/post_query
-# "query_graph": {
-#   "edges": [
-#     {
-#       "id": "e00",
-#       "source_id": "n00",
-#       "target_id": "n01",
-#       "type": "treated_by"
-#     }
-#   ],
-#   "nodes": [
-#     {
-#       "curie": "MONDO:0021668",
-#       "id": "n00",
-#       "type": "disease"
-#     },
-#     {
-#       "id": "n01",
-#       "type": "drug"
-#     }
-#   ]
-# }
-
-#   "results": [
-#     {
-#     "edge_bindings": [
-#         {
-#         "kg_id": "e0",
-#         "qg_id": "e00"
-#         }
-#     ],
-#     "node_bindings": [
-#         {
-#         "kg_id": "MONDO:0021668",
-#         "qg_id": "n00"
-#         },
-#         {
-#         "kg_id": "ChEMBL:CHEMBL2106966",
-#         "qg_id": "n01"
-#         }
-#     ]
-#     }
-#   ]
