5b1eaccc0c0d4eae3a3dbcac4466df8bd42cc5df
Vincent Emonet <vincent.emonet@gmail.com>
2021-06-21 12:48:58 +0200

    fix reasoner api

--- openpredict/reasonerapi_parser.py
+++ openpredict/reasonerapi_parser.py
@@ -227,7 +227,7 @@ def typed_results_to_reasonerapi(reasoner_query):
     # Generate kg nodes from the dict of nodes + result from query to resolve labels
     for node_id, properties in node_dict.items():
         node_to_add = {
-            'categories': 'biolink:' + properties['type'].capitalize() ,
+            'categories': ['biolink:' + properties['type'].capitalize()] ,
             }
         if 'label' in properties and properties['label']:
             node_to_add['name'] = properties['label']
