c71500415392ef45a2408348fd84f1ab304991e7
Vincent Emonet <vincent.emonet@gmail.com>
2022-03-15 15:20:02 +0100

    update deployment path

--- restart_staging.sh
+++ restart_staging.sh
@@ -4,4 +4,4 @@
 #git commit -m "Improvements"
 #git push
 
-ssh ids3 'cd /data/deploy-ids-tests/translator-openpredict ; git pull ; docker-compose -f docker-compose.yml -f docker-compose.staging.yml build ; docker-compose down ; docker-compose -f docker-compose.yml -f docker-compose.staging.yml up -d --force-recreate'
+ssh ids3 'cd /data/deploy-services/translator-openpredict ; git pull ; docker-compose -f docker-compose.yml -f docker-compose.staging.yml build ; docker-compose down ; docker-compose -f docker-compose.yml -f docker-compose.staging.yml up -d --force-recreate'
