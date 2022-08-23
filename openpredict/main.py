14d18d8ab501b75d47e568fdb84df47dcb678dad
Arif Yilmaz <arifyznl@gmail.com>
2022-08-24 00:12:11 +0200

    XPREDICT shapley based drug prediction explanation service for Translator Open Predict Tool

--- openpredict/main.py
+++ openpredict/main.py
@@ -8,7 +8,8 @@ from fastapi.responses import JSONResponse, RedirectResponse
 from openpredict.openapi import TRAPI, TRAPI_EXAMPLE, EmbeddingTypes, SimilarityTypes
 from openpredict.openpredict_model import (
     addEmbedding,
-    get_predictions,
+    get_predictions, 
+    get_explanations,
     get_similarities,
     load_similarity_embeddings,
 )
@@ -206,6 +207,59 @@ def get_predict(
     print('PredictRuntime: ' + str(datetime.now() - time_start))
     return {'hits': prediction_json, 'count': len(prediction_json)}
 
+@app.get("/explain", name="Get calculated shap explanations for  predicted drug targets for a given disease",
+    description="""Return the predicted targets for a given entity: drug (DrugBank ID) or disease (OMIM ID), with confidence scores.
+Only a drug_id or a disease_id can be provided, the disease_id will be ignored if drug_id is provided
+This operation is annotated with x-bte-kgs-operations, and follow the BioThings API recommendations.
+
+You can try:
+
+| disease_id: `OMIM:246300` | 
+
+| to check the drug prediction explanations  for a disease  |
+""",
+    response_model=dict,
+    tags=["biothings"],
+)
+def get_explanation(
+        #drug_id: Optional[str] = None, 
+        disease_id: Optional[str] = 'OMIM:246300', 
+        #model_id: str ='openpredict-baseline-omim-drugbank', 
+        n_results: int = 100
+    ) -> dict:
+    """Get explanations for a given entity CURIE disease and predicted drugs.
+
+    :param entity: Get explanations associations for this entity CURIE
+    :return: Prediction results with shap values for all features  in the  ML model with score
+    """
+    time_start = datetime.now()
+    #return ('test: provide a drugid or diseaseid', 400)
+    # TODO: if drug_id and disease_id defined, then check if the disease appear in the provided drug predictions
+    concept_id = ''
+    drug_id= None
+    model_id=None
+    min_score=None
+    max_score=None
+    if drug_id:
+        concept_id = drug_id
+    elif disease_id:
+        concept_id = disease_id
+    else:
+        return ('Bad request: provide a drugid or diseaseid', 400)
+
+    try:
+        
+        prediction_json, source_target_predictions = get_explanations(
+            concept_id, model_id, app, min_score, max_score, n_results
+        )
+        
+    except Exception as e:
+        print('Error processing ID ' + concept_id)
+        print(e)
+        return ('Not found: entry in OpenPredict for ID ' + concept_id, 404)
+
+    print('PredictRuntime: ' + str(datetime.now() - time_start))
+    return {'hits': prediction_json, 'count': len(prediction_json)}
 
 
 @app.get("/similarity", name="Get similar entities",
