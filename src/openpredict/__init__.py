f8f27205af623851ed1ffdfae2abab4043b0296b
Vincent Emonet <vincent.emonet@gmail.com>
2022-12-12 17:54:59 +0100

    update workflows names

--- src/openpredict/__init__.py
+++ src/openpredict/__init__.py
@@ -1,4 +1,4 @@
 from .decorators import trapi_predict
 from .predict_output import PredictHit, PredictOptions, PredictOutput, TrainingOutput, TrapiRelation
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
