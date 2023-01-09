aa7335015d77681f886ad11b904f9cdf4c5d6d99
Vincent Emonet <vincent.emonet@gmail.com>
2023-01-09 11:57:05 +0100

    move custom openpredict api to openpredict_model/api.py

--- src/openpredict_model/api.py
+++ src/openpredict_model/api.py
@@ -1,11 +1,16 @@
+import os
+import sys
 from datetime import datetime
 from enum import Enum
 from typing import Optional
 
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, File, Query, UploadFile
+from rdflib import Graph
 
+from openpredict.rdf_utils import retrieve_features, retrieve_models
 from openpredict_model.evidence_path.predict import do_evidence_path
 from openpredict_model.explain_shap.explain_shap import get_explanations
+from openpredict_model.train import add_embedding
 
 
 class FeatureTypesDrugs(str, Enum):
@@ -19,8 +24,16 @@ class FeatureTypesDiseases(str, Enum) :
     HPO_SIM = "HPO-SIM"
     PHENO_SIM = "PHENO-SIM"
 
+class EmbeddingTypes(str, Enum):
+    Both = "Both"
+    Drugs = "Drugs"
+    Diseases = "Diseases"
+
 
 api = APIRouter()
+
+models_g = Graph()
+models_g.parse("models/openpredict_baseline.ttl")
 
 
 @api.get("/evidence-path", name="Get the evidence path between two entities",
@@ -143,4 +156,73 @@ def get_explanation(
 
     print('PredictRuntime: ' + str(datetime.now() - time_start))
     return {'hits': prediction_json, 'count': len(prediction_json)}
-import sys
+
+
+
+@api.get("/features", name="Return the features trained in the models",
+    description="""Return the features trained in the model, for Drugs, Diseases or Both.""",
+    response_model=dict,
+    tags=["openpredict"],
+)
+def get_features(embedding_type: EmbeddingTypes ='Drugs') -> dict:
+    """Get features in the model
+
+    :return: JSON with features
+    """
+    if type(embedding_type) is EmbeddingTypes:
+        embedding_type = embedding_type.value
+    return retrieve_features(models_g, embedding_type)
+
+
+
+@api.get("/models", name="Return the models with their training features and scores",
+    description="""Return the models with their training features and scores""",
+    response_model=dict,
+    tags=["openpredict"],
+)
+def get_models() -> dict:
+    """Get models with their scores and features
+
+    :return: JSON with models and features
+    """
+    return retrieve_models(models_g)
+
+
+
+@api.post("/embedding", name="Upload your embedding for drugs or diseases",
+    description="""Upload your embedding file:
+
+1. Select which types do you have in the embeddings: Drugs, Diseases or Both.
+
+2. Define the base `model_id`: use the `/models` call to see the list of trained models with their characteristics, and pick the ID of the model you will use as base to add your embedding
+
+3. The model will be retrained and evaluation will be stored in a triplestore (available in `/models`)
+""",
+    response_model=dict,
+    tags=["openpredict"],
+)
+def post_embedding(
+        emb_name: str, description: str,
+        types: EmbeddingTypes ='Both', model_id: str ='openpredict_baseline',
+        apikey: str=None,
+        uploaded_file: UploadFile = File(...)
+    ) -> dict:
+    """Post JSON embeddings via the API, with simple APIKEY authentication
+    provided in environment variables
+    """
+    if type(types) is EmbeddingTypes:
+        types = types.value
+
+    # Ignore the API key check if no env variable defined (for development)
+    if os.getenv('OPENPREDICT_APIKEY') == apikey or os.getenv('OPENPREDICT_APIKEY') is None:
+        embedding_file = uploaded_file.file
+        run_id, loaded_model = add_embedding(
+            embedding_file, emb_name, types, model_id)
+        print('Embeddings uploaded')
+        # train_model(False)
+        return {
+            'status': 200,
+            'message': 'Embeddings added for run ' + run_id + ', trained model has scores ' + str(loaded_model.scores)
+        }
+    else:
+        return {'Forbidden': 403}
--- src/trapi/main.py
+++ src/trapi/main.py
@@ -1,25 +1,11 @@
 import logging
-import os
-from enum import Enum
-
-from fastapi import File, UploadFile
-from rdflib import Graph
 
 from drkg_model.api import api as drkg_model_api
 from openpredict.config import settings
-from openpredict.rdf_utils import retrieve_features, retrieve_models
 from openpredict.trapi import TRAPI
 from openpredict.utils import init_openpredict_dir
 from openpredict_model.api import api as openpredict_api
 from openpredict_model.predict import get_predictions, get_similarities
-from openpredict_model.train import add_embedding
-
-
-class EmbeddingTypes(str, Enum):
-    Both = "Both"
-    Drugs = "Drugs"
-    Diseases = "Diseases"
-
 
 # Other TRAPI project using FastAPI: https://github.com/NCATS-Tangerine/icees-api/blob/master/icees_api/trapi.py
 
@@ -34,9 +20,6 @@ predict_endpoints = [
     get_predictions,
     get_similarities,
 ]
-
-models_g = Graph()
-models_g.parse("models/openpredict_baseline.ttl")
 
 
 openapi_info = {
@@ -95,73 +78,3 @@ app = TRAPI(
 
 app.include_router(openpredict_api)
 app.include_router(drkg_model_api)
-
-
-
-@app.get("/features", name="Return the features trained in the models",
-    description="""Return the features trained in the model, for Drugs, Diseases or Both.""",
-    response_model=dict,
-    tags=["openpredict"],
-)
-def get_features(embedding_type: EmbeddingTypes ='Drugs') -> dict:
-    """Get features in the model
-
-    :return: JSON with features
-    """
-    if type(embedding_type) is EmbeddingTypes:
-        embedding_type = embedding_type.value
-    return retrieve_features(models_g, embedding_type)
-
-
-
-@app.get("/models", name="Return the models with their training features and scores",
-    description="""Return the models with their training features and scores""",
-    response_model=dict,
-    tags=["openpredict"],
-)
-def get_models() -> dict:
-    """Get models with their scores and features
-
-    :return: JSON with models and features
-    """
-    return retrieve_models(models_g)
-
-
-
-@app.post("/embedding", name="Upload your embedding for drugs or diseases",
-    description="""Upload your embedding file:
-
-1. Select which types do you have in the embeddings: Drugs, Diseases or Both.
-
-2. Define the base `model_id`: use the `/models` call to see the list of trained models with their characteristics, and pick the ID of the model you will use as base to add your embedding
-
-3. The model will be retrained and evaluation will be stored in a triplestore (available in `/models`)
-""",
-    response_model=dict,
-    tags=["openpredict"],
-)
-def post_embedding(
-        emb_name: str, description: str,
-        types: EmbeddingTypes ='Both', model_id: str ='openpredict_baseline',
-        apikey: str=None,
-        uploaded_file: UploadFile = File(...)
-    ) -> dict:
-    """Post JSON embeddings via the API, with simple APIKEY authentication
-    provided in environment variables
-    """
-    if type(types) is EmbeddingTypes:
-        types = types.value
-
-    # Ignore the API key check if no env variable defined (for development)
-    if os.getenv('OPENPREDICT_APIKEY') == apikey or os.getenv('OPENPREDICT_APIKEY') is None:
-        embedding_file = uploaded_file.file
-        run_id, loaded_model = add_embedding(
-            embedding_file, emb_name, types, model_id)
-        print('Embeddings uploaded')
-        # train_model(False)
-        return {
-            'status': 200,
-            'message': 'Embeddings added for run ' + run_id + ', trained model has scores ' + str(loaded_model.scores)
-        }
-    else:
-        return {'Forbidden': 403}
