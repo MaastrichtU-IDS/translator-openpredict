05b711ad12d7d83a97814cd54733bd938f1184f9
Vincent Emonet <vincent.emonet@gmail.com>
2024-03-25 11:22:22 +0100

    fix docker and add new drug-target ddl scripts

---
+++ predict-drug-target/scripts/getDrugCentral.sh
@@ -0,0 +1,2 @@
+wget -O ../data/download/drugcentral_drug_target.tsv.gz https://unmtid-shinyapps.net/download/DrugCentral/2021_09_01/drug.target.interaction.tsv.gz
+gunzip ../data/download/drugcentral_drug_target.tsv.gz
